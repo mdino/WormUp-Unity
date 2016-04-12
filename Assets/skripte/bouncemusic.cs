fa0ba8350892f23decb0479bf8a0811a84a1cca1
Dino <dinomusliu@Dinos-MacBook-Pro.local>
2016-04-12 19:40:05 +0200

    Verzija 1.3
    Verzija 1.3

---
+++ Assets/skripte/bouncemusic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bouncemusic : MonoBehaviour {
+
+	AudioClip[] Jumps;
+	public AudioClip Jump;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.tag == "Player") {
+			GetComponent<AudioSource>().PlayOneShot(Jump);
+		}
+	}
+}
